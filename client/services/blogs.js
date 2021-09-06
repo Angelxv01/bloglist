7f3a3ce1fb9ea9fe2b878c46d72f6a5ab7f4d7b8
Angel <angeloxv@outlook.it>
2021-09-06 18:48:25 +0200

    pipeline setting

--- client/services/blogs.js
+++ client/services/blogs.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = `http://localhost:5000/api/blogs`;
+const baseUrl = "/api/blogs";
 
 let token = null;
 
--- index.js
+++ index.js
@@ -3,5 +3,7 @@ const { PORT } = require("./config");
 const app = require("./app");
 const logger = require("./server/utils/logger");
 
+const port = PORT || 5000;
+
 const server = http.createServer(app);
-server.listen(PORT, () => logger.Info(`Server running on port ${PORT}`));
+server.listen(port, () => logger.Info(`Server running on port ${PORT}`));
